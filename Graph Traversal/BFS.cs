9acecfdd2d1782b5ca64d0eeae5da215e3cf62d1
Mehedi Hasan Sami <mhsami53oleander@gmail.com>
2022-03-28 00:35:32 +0600

    BFS Added

---
+++ Graph Traversal/BFS.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graph_Traversal
+{
+    class Parent_Graph
+    {
+        int number_of_vertices;
+        LinkedList<Int32>[] adj;
+        public Parent_Graph(int number)
+        {
+            this.number_of_vertices = number;
+            adj = new LinkedList<Int32>[number_of_vertices];
+            for (int i = 0; i < number_of_vertices; i++)
+                adj[i] = new LinkedList<Int32>();
+        }
+        public void addEdge(int from, int to)
+        {
+            adj[from].AddLast(to);
+            adj[to].AddLast(from); //// if Undirectional graph
+        }
+        public void Print_Graph()
+        {
+            for (int i = 0; i < adj.Length; i++)
+            {
+                Console.Write(i + "-->");
+                foreach (int p in adj[i])
+                    Console.Write(p + " ");
+                Console.WriteLine();
+            }
+        }
+        public void BFS(int source, int destination)
+        {
+            int[] parent = new int[number_of_vertices];
+            bool[] visited = new bool[number_of_vertices];
+            visited[source] = true;
+            parent[source] = -1;
+            Queue<int> st = new Queue<int>();
+            st.Enqueue(source);
+            while (st.Count() != 0)
+            {
+                int node = st.Dequeue();
+                if (node == destination)
+                    break;
+                else
+                {
+                    foreach (int neighbour in adj[node])
+                    {
+                        if (!visited[neighbour])
+                        {
+                            visited[neighbour] = true;
+                            parent[neighbour] = node;
+                            st.Enqueue(neighbour);
+                        }
+                    }
+
+                }
+            }
+            print_path(parent,destination);
+        }
+
+        private void print_path(int[] parent,int destination)
+        {
+            int current = destination;
+            while(parent[current]!=-1)
+            {
+                Console.WriteLine("Parent of {0} is {1}", current, parent[current]);
+                current = parent[current];
+            }
+            Console.WriteLine("Source is {0}", current);
+        }
+    }
+}
---
+++ Graph Traversal/Graph.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graph_Traversal
+{
+    internal class Graph
+    {
+        static void Main(string[] args)
+        {
+            int vertices, edges;
+            vertices = Convert.ToInt32(Console.ReadLine());
+            edges    = Convert.ToInt32(Console.ReadLine());
+            Parent_Graph G = new Parent_Graph(vertices);
+            //for (int i = 0; i < edges; i++)
+            //{
+            //    int from = Convert.ToInt32(Console.ReadLine());
+            //    int to = Convert.ToInt32(Console.ReadLine());
+            //    G.addEdge(from, to);
+            //}
+            G.addEdge(0, 4);
+            G.addEdge(0, 3);
+            G.addEdge(1, 2);
+            G.addEdge(1, 4);
+            G.addEdge(2, 3);
+            G.addEdge(3, 4);
+
+            G.Print_Graph();
+
+            G.BFS(2,4);    // source =2 destination= 4
+            Console.ReadLine();
+        }
+    }
+}
